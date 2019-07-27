c861d11dfa1aa2c568232009b0b84a2c03687c32
Jonathan <faustin.jonathan@gmail.com>
2019-07-27 21:08:23 +0200

    Refactor apple class to inherit from sprite

--- components/apple.py
+++ components/apple.py
@@ -1,22 +1,37 @@
-import pygame
+import os, pygame
+from pygame.compat import geterror
+from pygame.locals import *
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(main_dir, '../graphics')
 
 
-class Apple():
-    # Singleton pattern
-    class __Apple():
-        def __init__(self, position):
-            self.position = position
-            self.image = pygame.image.load(
-                "/home/jonathan/Projects/PyGames/Snake/graphics/apple_simple.png"
-            )
+# functions to create our resources
+def load_image(name, colorkey=None, rescale=True):
+    fullname = os.path.join(data_dir, name)
+    try:
+        image = pygame.image.load(fullname)
+        if rescale:
+            image = pygame.transform.scale(image, (20, 20))
+    except pygame.error:
+        print('Cannot load image:', fullname)
+        raise SystemExit(str(geterror()))
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
 
-    instance = None
 
-    def __init__(self, position):
-        if not Apple.instance:
-            Apple.instance = Apple.__Apple(position)
-        else:
-            Apple.instance.position = position
+class Apple(pygame.sprite.Sprite):
+    """Apple to feed the snake"""
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.image, self.rect = load_image("apple_alpha.png", -1)
+#        self.image = pygame.transform.scale(image, (20, 20))
 
-    def show(self, game_display):
-        game_display.blit(self.instance.image, self.instance.position)
+    def move(self, position):
+        self.rect.midtop = position
--- main.py
+++ main.py
@@ -7,27 +7,35 @@ height = 600
 
 def main():
     pygame.init()
-    game_display = pygame.display.set_mode((width, height))
+    screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('HELLO')
     clock = pygame.time.Clock()
 
     crashed = False
 
+    # Create The Backgound
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((250, 250, 250))
+
+    apple = Apple()
+    allsprites = pygame.sprite.RenderPlain((apple))
+
     while not crashed:
-        game_display.fill((255, 255, 255))
+        clock.tick(60)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 crashed = True
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
-                apple = Apple(pos)
+                apple.move(pos)
 
-        if Apple.instance:
-            apple.show(game_display)
-        pygame.display.update()
-
-        clock.tick(60)
+        allsprites.update()
+        # Draw Everything
+        screen.blit(background, (0, 0))
+        allsprites.draw(screen)
+        pygame.display.flip()
 
     pygame.quit()
     quit()
