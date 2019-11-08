73a0cec5f4a6babe81a3f886d7b8912120aa8b89
Jonathan <faustin.jonathan@gmail.com>
2019-11-08 21:21:57 +0100

    WIP: correcting genetic algo

--- neural_net/genetic_algorithm.py
+++ neural_net/genetic_algorithm.py
@@ -4,22 +4,27 @@ import itertools
 import numpy as np
 from copy import deepcopy
 
-
-def select_best_parents(path, gen, nb_best):
+# TODO: Refactor this shit
+def initialise_pop_with_best_parents(path, gen, nb_best):
     p = path.glob('data_' + str(gen) + '_*.pickle')
     files = sorted([x for x in p if x.is_file()])
     f_data = OrderedDict()
     for nb, f in enumerate(files):
-        d = pickle.load(open(f, "rb"))
-        f_data[nb] = d[0]
+        dd = pickle.load(open(f, "rb"))
+        f_data[nb] = dd[0]
     sorted_x = sorted(f_data.items(), key=lambda kv: kv[1], reverse=True)
     sorted_dict = OrderedDict(sorted_x)
-    data = []
-    for nb in sorted_dict.keys():
+    data = deepcopy(dd[1:])
+    ####### TODO FIX HERE
+    data = [data[:]*0]*len(files)
+    id_best = []
+    import pdb; pdb.set_trace()
+    for nb in list(sorted_dict.keys())[:nb_best]:
         f = path / 'data_{}_{}.pickle'.format(gen, nb)
-        d = pickle.load(open(f, "rb"))
-        data.append(d[1:])
-    return data[:nb_best]
+        dd = pickle.load(open(f, "rb"))
+        data[nb, :, :, :] = dd[1], dd[2], dd[3]
+        id_best.append(nb)
+    return id_best, data
 
 
 def cross_over(data1, data2):
@@ -27,7 +32,7 @@ def cross_over(data1, data2):
     randR = np.random.randint(len(data1))
     for i in range(len(data1)):
         if i > randR:
-            result[i] = data2[i]
+            result[i:i + 1] = data2[i:i + 1]
     return result
     # return np.concatenate((data1[0:len(data1) // 2], data2[len(data2) // 2:]),
     #                       axis=None)
@@ -60,24 +65,33 @@ def generate_child(best_parents):
         new_w2 = cross_over(par1[1].flatten(),
                             par2[1].flatten()).reshape(w2_shape)
         new_b = cross_over(par1[2].flatten(), par2[2].flatten())
+        import pdb
+        pdb.set_trace()
         new_w1 = mutate(new_w1)
         new_w2 = mutate(new_w2)
         new_b = mutate(new_b)
+        import pdb
+        pdb.set_trace()
         yield (new_w1, new_w2, new_b)
 
 
 def generate_new_population(path, gen, nb_pop, nb_best=4):
-    new_pop = select_best_parents(path, gen, nb_best)
-    while True:
-        child = generate_child(new_pop)
-        new_pop.append(next(child))
-        if len(new_pop) == nb_pop:
-            return new_pop
+    id_best, new_pop = initialise_pop_with_best_parents(path, gen, nb_best)
+    children = generate_child(new_pop[id_best])
+    for ii in range(len(new_pop)):
+        if ii not in id_best:
+
+            new_pop[ii] = next(children)
+    # while len(new_pop) < nb_pop:
+    #     child = generate_child(new_pop)
+    #     new_pop.append(next(child))
+    return new_pop
 
 
 if __name__ == '__main__':
     nb_best = 10
     from pathlib import Path
-    # best_parents = list(select_best_parents(Path("../genetic_data/."), 0, 4))
+    # best_parents = list(initialise_pop_with_best_parents(Path("../genetic_data/."), 0, 4))
     # children = generate_child(best_parents)
-    print(generate_new_population(Path("../genetic_data/."), 0, nb_best=4))
+    print(generate_new_population(Path("../genetic_data/."), 0, 100,
+                                  nb_best=4))
--- stats/stats.py
+++ stats/stats.py
@@ -15,8 +15,31 @@ def show_stats(all_score):
 def show_fitness(all_fitness):
     fig = plt.figure()
     ax = plt.gca()
-    im = plt.imshow(all_fitness, origin="lower")
+    im = plt.imshow(all_fitness.T, origin="lower")
     fig.colorbar(im, ax=ax)
-    plt.xlabel("games")
-    plt.ylabel("generation")
+    plt.ylabel("games")
+    plt.xlabel("generation")
     plt.show()
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+    import pickle
+    from collections import OrderedDict
+    gen = 9
+    path = Path("../genetic_data/.")
+    p = path.glob('data_' + str(gen) + '_*.pickle')
+    files = sorted([x for x in p if x.is_file()])
+    f_data = OrderedDict()
+    for nb, f in enumerate(files):
+        d = pickle.load(open(f, "rb"))
+        f_data[nb] = d[0]
+    sorted_x = sorted(f_data.items(), key=lambda kv: kv[1], reverse=True)
+    sorted_dict = OrderedDict(sorted_x)
+    data = []*nb_best
+    for nb in sorted_dict.keys():
+        f = path / 'data_{}_{}.pickle'.format(gen, nb)
+        d = pickle.load(open(f, "rb"))
+        data[nb] = d[1:]
+    import pdb; pdb.set_trace()
+    
