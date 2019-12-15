3602dd3f275629138a76275d738bb08cf6c36ea6
Jonathan <faustin.jonathan@gmail.com>
2019-12-15 09:38:14 +0100

    Correct and complete tests on food detection

--- tests/test_snake.py
+++ tests/test_snake.py
@@ -62,28 +62,67 @@ def test_get_next_pos_right():
 
 
 def test_is_food_ahead():
+    # Snake going up
     snake = Snake(0, 40, (0, -BASE_SPEED))
     assert snake.is_food_ahead(apple_pos)
+    # Snake going down
     snake = Snake(0, 0, (0, BASE_SPEED))
     assert snake.is_food_ahead(apple_pos)
-
-    snake = Snake(0, 20, (BASE_SPEED, 0))
-    assert snake.is_food_ahead(apple_pos)
-    snake = Snake(40, 20, (-BASE_SPEED, 0))
-    assert snake.is_food_ahead(apple_pos)
-
-
-def test_is_food_right():
-    # Snake going up
-    snake = Snake(20, 0, (BASE_SPEED, 0))
-    assert snake.is_food_right(apple_pos)
-    # Snake going down
-    snake = Snake(10, 0, (0, BASE_SPEED))
-    assert snake.is_food_ahead(apple_pos)
-
     # Snake going right
     snake = Snake(0, 20, (BASE_SPEED, 0))
     assert snake.is_food_ahead(apple_pos)
     # Snake going left
     snake = Snake(40, 20, (-BASE_SPEED, 0))
     assert snake.is_food_ahead(apple_pos)
+
+
+def test_is_food_right():
+    # Snake going down
+    snake = Snake(30, 0, (0, BASE_SPEED))
+    assert snake.is_food_right(apple_pos)
+    snake = Snake(0, 0, (0, BASE_SPEED))
+    assert not snake.is_food_right(apple_pos)
+
+    # Snake going up
+    snake = Snake(0, 30, (0, -BASE_SPEED))
+    assert snake.is_food_right(apple_pos)
+    snake = Snake(30, 30, (0, -BASE_SPEED))
+    assert not snake.is_food_right(apple_pos)
+
+    # Snake going right
+    snake = Snake(0, 0, (BASE_SPEED, 0))
+    assert snake.is_food_right(apple_pos)
+    snake = Snake(0, 30, (BASE_SPEED, 0))
+    assert not snake.is_food_right(apple_pos)
+
+    # Snake going left
+    snake = Snake(30, 30, (-BASE_SPEED, 0))
+    assert snake.is_food_right(apple_pos)
+    snake = Snake(30, 0, (-BASE_SPEED, 0))
+    assert not snake.is_food_right(apple_pos)
+
+
+def test_is_food_left():
+    # Snake going down
+    snake = Snake(0, 0, (0, BASE_SPEED))
+    assert snake.is_food_left(apple_pos)
+    snake = Snake(30, 0, (0, BASE_SPEED))
+    assert not snake.is_food_left(apple_pos)
+
+    # Snake going up
+    snake = Snake(30, 30, (0, -BASE_SPEED))
+    assert snake.is_food_left(apple_pos)
+    snake = Snake(0, 30, (0, -BASE_SPEED))
+    assert not snake.is_food_left(apple_pos)
+
+    # Snake going right
+    snake = Snake(0, 30, (BASE_SPEED, 0))
+    assert snake.is_food_left(apple_pos)
+    snake = Snake(0, 0, (BASE_SPEED, 0))
+    assert not snake.is_food_left(apple_pos)
+
+    # Snake going left
+    snake = Snake(30, 0, (-BASE_SPEED, 0))
+    assert snake.is_food_left(apple_pos)
+    snake = Snake(30, 30, (-BASE_SPEED, 0))
+    assert not snake.is_food_left(apple_pos)
