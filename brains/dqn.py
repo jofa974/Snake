Fix q values calculation

brains/dqn.py
@@ -34,32 +34,34 @@ class DQN(Brain):
 
     def select_action(self, state, epsilon):
         if random.random() < epsilon:
-            action = random.choice(range(3))
+            return random.choice(range(3))
         else:
             probs = F.softmax(self.model(state), dim=1)
-            action = probs.multinomial(num_samples=1)[0][0].item()
+            action = probs.multinomial(num_samples=1)[0][0]
+            return action.item()
+
+    def action2direction_key(self, action):
         directions = ["forward", "left", "right"]
         if directions[action] == "forward":
-            action = pygame.K_SPACE
+            return pygame.K_SPACE
         elif directions[action] == "left":
             if self.snake.speed[0] > 0:
-                action = pygame.K_UP
+                return pygame.K_UP
             if self.snake.speed[0] < 0:
-                action = pygame.K_DOWN
+                return pygame.K_DOWN
             if self.snake.speed[1] > 0:
-                action = pygame.K_RIGHT
+                return pygame.K_RIGHT
             if self.snake.speed[1] < 0:
-                action = pygame.K_LEFT
+                return pygame.K_LEFT
         elif directions[action] == "right":
             if self.snake.speed[0] > 0:
-                action = pygame.K_DOWN
+                return pygame.K_DOWN
             if self.snake.speed[0] < 0:
-                action = pygame.K_UP
+                return pygame.K_UP
             if self.snake.speed[1] > 0:
-                action = pygame.K_LEFT
+                return pygame.K_LEFT
             if self.snake.speed[1] < 0:
-                action = pygame.K_RIGHT
-        return action
+                return pygame.K_RIGHT
 
     @abstractmethod
     def learn(self, batch_state, batch_next_state, batch_reward, batch_action):

brains/dqn_cnn.py
@@ -67,14 +67,15 @@ class DQN_CNN(DQN):
             self.env.make_surf_from_figure_on_canvas(fig)
 
             last_signal = self.get_input_data()
-            next_move = self.update(
+            next_action = self.update(
                 self.last_reward,
                 last_signal,
-                batch_size=100,
+                batch_size=10,
                 nb_steps=nb_moves,
                 epsilon=epsilon,
             )
 
+            next_move = self.action2direction_key(next_action)
             if next_move in ui.CONTROLS:
                 self.snake.change_direction(next_move)
 
@@ -123,10 +124,7 @@ class DQN_CNN(DQN):
         plt.clf()
         ax = plt.gca()
         ax.scatter(
-            np.arange(len(self.loss_history)),
-            np.array(self.loss_history),
-            s=20,
-            c="r",
+            np.arange(len(self.loss_history)), np.array(self.loss_history), s=20, c="r",
         )
         # ax.set_aspect("equal", adjustable="box")
         # self.make_surf_from_figure_on_canvas(fig)
@@ -141,7 +139,9 @@ class DQN_CNN(DQN):
         return arr
 
     def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
-        outputs = self.model(batch_state).max(1)[0]
+        outputs = (
+            self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        )
         next_outputs = self.model(batch_next_state).detach().max(1)[0]
         targets = batch_reward + self.gamma * next_outputs
         loss = self.loss(outputs, targets)